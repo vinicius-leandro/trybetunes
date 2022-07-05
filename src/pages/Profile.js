d201e408445b4b17fdbc69f57b34dea6e90a5345
vinicius leandro <leandrocostavinicius@gmail.com>
2022-07-05 20:46:55 -0300

    Requisito 13

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,13 +1,51 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Header from '../components/Header';
+import Loading from '../components/Loading';
+import { getUser } from '../services/userAPI';
+import ProfileCard from '../components/ProfileCard';
 
 class Profile extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      profile: [],
+      loading: true,
+      successfulFetch: false,
+    };
+  }
+
+  componentDidMount() {
+    this.getProfile();
+  }
+
+  getProfile = async () => {
+    const result = await getUser();
+    this.setState({ profile: result, loading: false, successfulFetch: true });
+  }
+
   render() {
     const { history } = this.props;
+    const {
+      profile: { image, name, email, description },
+      loading,
+      successfulFetch } = this.state;
     return (
       <div data-testid="page-profile">
         <Header history={ history } />
+        { loading && <Loading /> }
+        <section>
+          {
+            successfulFetch && (
+              <ProfileCard
+                image={ image }
+                name={ name }
+                email={ email }
+                description={ description }
+              />
+            )
+          }
+        </section>
       </div>
     );
   }
